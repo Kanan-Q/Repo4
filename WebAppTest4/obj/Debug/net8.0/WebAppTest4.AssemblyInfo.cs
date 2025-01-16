a62f2aeb0b6549f48d3d4c15d2ed714f2f122ace
crispyyy-365 <huseynaa-bp217@code.edu.az>
2025-01-16 17:56:14 +0400

    no Commit

--- WebAppTest4/obj/Debug/net8.0/WebAppTest4.AssemblyInfo.cs
+++ WebAppTest4/obj/Debug/net8.0/WebAppTest4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTest4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6cc504349891dd7ea58b913c759bdbec54e4b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTest4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTest4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
